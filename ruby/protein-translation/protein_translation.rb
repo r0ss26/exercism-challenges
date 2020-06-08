f5bb53931d2ce5f61305ae82f31d6816b71f0643
Ross Baker <rossbaker32@gmail.com>
2020-06-08 21:19:22 +1000

    Refactor protein translation

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -9,36 +9,35 @@ To get started with TDD, see the `README.md` file in your
 class Translation
   def self.of_codon(codon)
     codon_to_polypeptide = {
-      AUG: :Methionine,
-      UUU: :Phenylalanine,
-      UUC: :Phenylalanine,
-      UUA: :Leucine,
-      UUG: :Leucine,
-      UCU: :Serine,
-      UCC: :Serine,
-      UCA: :Serine,
-      UCG: :Serine,
-      UAU: :Tyrosine, 
-      UAC: :Tyrosine,
-      UGU: :Cysteine,
-      UGC: :Cysteine,
-      UGG: :Tryptophan,
-      UAA: :STOP,
-      UAG: :STOP,
-      UGA: :STOP
+      AUG: "Methionine",
+      UUU: "Phenylalanine",
+      UUC: "Phenylalanine",
+      UUA: "Leucine",
+      UUG: "Leucine",
+      UCU: "Serine",
+      UCC: "Serine",
+      UCA: "Serine",
+      UCG: "Serine",
+      UAU: "Tyrosine", 
+      UAC: "Tyrosine",
+      UGU: "Cysteine",
+      UGC: "Cysteine",
+      UGG: "Tryptophan",
+      UAA: "STOP",
+      UAG: "STOP",
+      UGA: "STOP"
     }
 
-    return codon_to_polypeptide[codon.to_sym].to_s
+    return codon_to_polypeptide[codon.to_sym]
   end
 
   def self.of_rna(rna_strand)
     protein_array = []
     i = 0
-    codon = nil
     while i < rna_strand.length - 1 do
       codon = rna_strand.slice(i, 3)
 
-      raise InvalidCodonError.new if self.of_codon(codon) == ""
+      raise InvalidCodonError.new if !self.of_codon(codon)
 
       break if self.of_codon(codon.to_sym) == "STOP"
       protein_array.push(self.of_codon(codon)) 
